418ed3e5c163241bd79ab30e10c6253553529efb
aleut <turbomonkeyninja@gmail.com>
2017-07-04 22:33:27 -0600

    saved corpus and dict

--- data_clean.py
+++ data_clean.py
@@ -1,5 +1,3 @@
-import os
-import pickle
 from gensim import corpora
 from load_bbc_articles import load_all_bbc_articles
 
@@ -8,16 +6,16 @@ def main():
     # Load texts from bbc articles
     articles = load_all_bbc_articles()
     full_texts = [a[2] for a in articles]
-    # Clean stuff like the gensim quickstart tutorial
+    # Remove stopwords
     with open('stopwords.txt', 'r') as f:
         stopwords = [w.strip() for w in f.readlines()]
-    # Remove stopwords
     texts = [[w for w in text.lower().split() if w not in stopwords]
              for text in full_texts]
+    # Compute dictionary and corpus and save for reuse
     dictionary = corpora.Dictionary(texts)
-    dictionary.save('/tmp/articles.dict')
-    print(dictionary)
-
+    dictionary.save('tmp/articles.dict')
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    corpora.MmCorpus.serialize('tmp/articles.mm', corpus)
 
 
 if __name__ == '__main__':
