bfefc9520b26975f5cf7fb33fa9836a0f927283e
aleut <turbomonkeyninja@gmail.com>
2017-07-05 19:33:14 -0600

    initial query, does not seem to give consistent results

--- similarity_explore.py
+++ similarity_explore.py
@@ -1,21 +1,31 @@
 import os
 from gensim import corpora, models, similarities
 from config import articles_mm, articles_dict, articles_index
+from load_bbc_articles import load_all_bbc_articles
 
 
 def main():
+    dictionary = corpora.Dictionary.load(articles_dict)
+    corpus = corpora.MmCorpus(articles_mm)
+    lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
+    index = load_index(corpus, lsi, articles_index)
 
-    index = load_index(articles_dict, articles_mm, articles_index)
+    q1 = make_query_vec(dictionary, lsi, 'the stock market is for cool teens')
+    sims = index[q1]
+    sims = sorted(enumerate(sims), key=lambda item: -item[1])
+    articles = load_all_bbc_articles()
+    print(sims[0][0])
+    print(articles[sims[0][0]])
+    print(sims)
+    print()
 
+def make_query_vec(dictionary, lsi, query_text):
+    vec_bow = dictionary.doc2bow(query_text.lower().split())
+    return lsi[vec_bow]
 
-def load_index(dictionary, mm, index_path):
+def load_index(corpus, lsi, index_path):
     """ Load a matrix similarity index from a given dictionary and corpus
     """
-    dictionary = corpora.Dictionary.load(dictionary)
-    corpus = corpora.MmCorpus(mm)
-    # 2-dimensional LSI space
-    lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
-    # Cache the index
     if not os.path.isfile(index_path):
         index = similarities.MatrixSimilarity(lsi[corpus])
         index.save(index_path)
