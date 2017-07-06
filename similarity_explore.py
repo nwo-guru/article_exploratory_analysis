505d10d7d110b6a722c6a6fecaf2fe713251b156
aleut <turbomonkeyninja@gmail.com>
2017-07-05 19:37:48 -0600

    prints top 100 similiar results, results do not make much sense

--- similarity_explore.py
+++ similarity_explore.py
@@ -8,30 +8,21 @@ def main():
     dictionary = corpora.Dictionary.load(articles_dict)
     corpus = corpora.MmCorpus(articles_mm)
     lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
-    index = load_index(corpus, lsi, articles_index)
+    index = similarities.MatrixSimilarity(lsi[corpus])
 
-    q1 = make_query_vec(dictionary, lsi, 'the stock market is for cool teens')
+    q1 = make_query_vec(dictionary, lsi, 'china')
     sims = index[q1]
     sims = sorted(enumerate(sims), key=lambda item: -item[1])
     articles = load_all_bbc_articles()
     print(sims[0][0])
-    print(articles[sims[0][0]])
+    for n in range(100):
+        print(articles[sims[n][0]])
     print(sims)
     print()
 
 def make_query_vec(dictionary, lsi, query_text):
     vec_bow = dictionary.doc2bow(query_text.lower().split())
     return lsi[vec_bow]
-
-def load_index(corpus, lsi, index_path):
-    """ Load a matrix similarity index from a given dictionary and corpus
-    """
-    if not os.path.isfile(index_path):
-        index = similarities.MatrixSimilarity(lsi[corpus])
-        index.save(index_path)
-    else:
-        index = similarities.MatrixSimilarity.load(index_path)
-    return index
 
 if __name__ == '__main__':
     main()
